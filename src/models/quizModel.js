7da8a94cd1136488e6f68d72cc79c631058a277b
MickaelaRodrigues <mickaela.rodrigues@sptech.school>
2026-05-10 20:43:46 -0300

    Iniciando contrução da dashboard

--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -13,13 +13,11 @@ function listar() {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrarEstilo(estilo, fk_usuario) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",  estilo, fk_usuario);
+function cadastrar_usuario_estilo(estilo, fk_usuario, pontos) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente.");
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores     
-    //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO estilo (estilo, fk_usuario) VALUES ('${estilo}', '${fk_usuario}');
+    
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
