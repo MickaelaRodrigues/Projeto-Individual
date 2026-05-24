146d4daee9de15a43a2c255b6bac30e08da2cb8b
MickaelaRodrigues <mickaela.rodrigues@sptech.school>
2026-05-24 01:08:50 -0300

    Inserindo rotas e estilização dos cards

--- src/controllers/avisoController.js
+++ src/controllers/avisoController.js
@@ -1,18 +1,6 @@
 var avisoModel = require("../models/avisoModel");
 
-// function listar(req, res) {
-//     avisoModel.listar().then(function (resultado) {
-//         if (resultado.length > 0) {
-//             res.status(200).json(resultado);
-//         } else {
-//             res.status(204).send("Nenhum resultado encontrado!")
-//         }
-//     }).catch(function (erro) {
-//         console.log(erro);
-//         console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
-//         res.status(500).json(erro.sqlMessage);
-//     });
-// }
+
 
 function listarPorUsuario(req, res) {
     var idUsuario = req.params.idUsuario;
@@ -39,30 +27,10 @@ function listarPorUsuario(req, res) {
         );
 }
 
-// function pesquisarDescricao(req, res) {
-//     var descricao = req.params.descricao;
 
-//     avisoModel.pesquisarDescricao(descricao)
-//         .then(
-//             function (resultado) {
-//                 if (resultado.length > 0) {
-//                     res.status(200).json(resultado);
-//                 } else {
-//                     res.status(204).send("Nenhum resultado encontrado!");
-//                 }
-//             }
-//         ).catch(
-//             function (erro) {
-//                 console.log(erro);
-//                 console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
-//                 res.status(500).json(erro.sqlMessage);
-//             }
-//         );
-// }
-
-// feito
 
 function publicar(req, res) {
+
     var nomeMusica = req.body.nomeMusica;
     var cantor = req.body.cantor;
     var descricao = req.body.descricao;
@@ -99,30 +67,12 @@ function publicar(req, res) {
     }
 }
 
-function editar(req, res) {
-    var novaDescricao = req.body.descricao;
-    var idAviso = req.params.idAviso;
 
-    avisoModel.editar(novaDescricao, idAviso)
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        )
-        .catch(
-            function (erro) {
-                console.log(erro);
-                console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-
-}
 
 function deletar(req, res) {
-    var idAviso = req.params.idAviso;
-
-    avisoModel.deletar(idAviso)
+    var idUsuario = req.params.idUsuario;
+    var idMusica = req.params.idMusica;
+    avisoModel.deletar(idMusica,idUsuario)
         .then(
             function (resultado) {
                 res.json(resultado);
@@ -140,6 +90,5 @@ function deletar(req, res) {
 module.exports = {
     listarPorUsuario,
     publicar,
-    editar,
     deletar
 }
--- src/models/avisoModel.js
+++ src/models/avisoModel.js
@@ -1,50 +1,14 @@
 var database = require("../database/config");
 
-// function listar() {
-//     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-//     var instrucaoSql = `
-//         SELECT 
-//             a.id AS idAviso,
-//             a.titulo,
-//             a.descricao,
-//             a.fk_usuario,
-//             u.id AS idUsuario,
-//             u.nome,
-//             u.email,
-//             u.senha
-//         FROM aviso a
-//             INNER JOIN usuario u
-//                 ON a.fk_usuario = u.id;
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
 
-// function pesquisarDescricao(texto) {
-//     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
-//     var instrucaoSql = `
-//         SELECT 
-//             a.id AS idAviso,
-//             a.titulo,
-//             a.descricao,
-//             a.fk_usuario,
-//             u.id AS idUsuario,
-//             u.nome,
-//             u.email,
-//             u.senha
-//         FROM aviso a
-//             INNER JOIN usuario u
-//                 ON a.fk_usuario = u.id
-//         WHERE a.descricao LIKE '${texto}';
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
+
+
 
 function listarPorUsuario(idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
     var instrucaoSql = `
-    SELECT nomeMusica as MÚSICA,
+    SELECT  idMusica as id,
+        nomeMusica as MÚSICA,
         cantor as ARTISTA,
         descricao AS DESCRIÇÃO,
         ano AS ANO,
@@ -57,7 +21,6 @@ function listarPorUsuario(idUsuario) {
     return database.executar(instrucaoSql);
 }
 
-// fazendo
 
     function publicar(nomeMusica, cantor,descricao, tipoSentimento,anoCard,idUsuario) {
         console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", nomeMusica, cantor,descricao, tipoSentimento,anoCard,idUsuario);
@@ -68,19 +31,12 @@ function listarPorUsuario(idUsuario) {
         return database.executar(instrucaoSql);
     }
 
-function editar(novaDescricao, idAviso) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novaDescricao, idAviso);
-    var instrucaoSql = `
-        UPDATE aviso SET descricao = '${novaDescricao}' WHERE id = ${idAviso};
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
 
-function deletar(idAviso) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():", idAviso);
+
+function deletar(idMusica, idUsuario) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():", idMusica,idUsuario);
     var instrucaoSql = `
-        DELETE FROM aviso WHERE id = ${idAviso};
+        delete from musicas where idMusica = ${idMusica} and fk_usuario = ${idUsuario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -89,6 +45,5 @@ function deletar(idAviso) {
 module.exports = {
     listarPorUsuario,
     publicar,
-    editar,
     deletar
 }
